2b93f14ecf0959241e20feaa16391d5645342184
Benjamin Chanudet <hello@benjaminchanudet.com>
2019-11-01 00:46:49 +0100

    Move queries definitions into their own files

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,16 @@
 import { Callback, Logger, IPluginAuth } from '@verdaccio/types';
 import * as mysql from 'mysql';
-
-class MysqlQueries {
-
-    public authenticate : string;
-    public add_user : string;
-    public update_user : string;
-
-    constructor(config : MysqlQueries){
-        console.log(config, typeof config);
-        this.add_user = config.add_user !== undefined ? config.add_user : 'INSERT INTO `users`(`username`,`password`) VALUES(?, PASSWORD(?));';
-        this.update_user = config.update_user !== undefined ? config.update_user : 'UPDATE `users` SET `password`=? WHERE `username`=?;';
-        this.authenticate = config.authenticate !== undefined ? config.authenticate : `
-            SELECT
-                u.\`username\`,
-                GROUP_CONCAT(g.\`name\`) AS usergroups
-            FROM
-                \`users\` u
-            LEFT JOIN \`user_group\` ug
-                ON ug.\`username\` = u.\`username\`
-            LEFT JOIN \`groups\` g
-                ON ug.\`group\` = g.\`name\`
-            WHERE
-                u.\`username\` = ?
-                AND u.\`password\` = PASSWORD(?)
-        `;
-    }
-}
+import { IMysqlQueries, MysqlQueries } from './mysql-queries';
 
 interface MysqlAuthConfig {
     connection : mysql.ConnectionConfig;
-    customQueries : MysqlQueries;
+    customQueries : IMysqlQueries;
 }
 
 export default class MysqlAuth  implements IPluginAuth<MysqlAuthConfig> {
 
     private config : mysql.ConnectionConfig;
-    private queries : MysqlQueries;
+    private queries : IMysqlQueries;
 
     private logger: Logger;
     
@@ -51,13 +25,13 @@ export default class MysqlAuth  implements IPluginAuth<MysqlAuthConfig> {
     authenticate(user: string, password: string, cb: Callback){
         const connection = mysql.createConnection(this.config);
 
-        if(this.queries.authenticate.length == 0){
+        if(this.queries.auth_user.length == 0){
             this.logger.info('MySQL - Can\'t authenticate : authenticate query is empty');
             cb(null, false)
             return;
         }
 
-        connection.query(this.queries.authenticate,[user, password], (error, result) => {
+        connection.query(this.queries.auth_user,[user, password], (error, result) => {
             if(error || result.length !== 1 || result[0].usergroups === null){
                 cb(null, false);
             }
---
+++ src/mysql-queries.ts
@@ -0,0 +1,114 @@
+
+export interface IMysqlQueries{
+    readonly add_user: string;
+    readonly update_user: string;
+    readonly auth_user: string;
+    readonly init_database: string[];
+}
+
+export class MysqlQueries implements IMysqlQueries{
+
+    constructor(private custom : IMysqlQueries){
+
+    }
+
+    public get add_user() : string{
+        if(this.custom.add_user !== undefined){
+            return this.custom.add_user;
+        }
+
+        return this._default_add_user;
+    }
+
+    public get update_user() : string{
+        if(this.custom.update_user !== undefined){
+            return this.custom.update_user;
+        }
+        return this._default_update_user;
+    }
+
+    public get auth_user(): string{
+        if(this.custom.auth_user !== undefined){
+            return this.custom.auth_user;
+        }
+        return this._default_auth_user;
+    }
+
+    public get init_database(): string[]{
+        if(this.custom.auth_user !== undefined){
+            return this.custom.init_database;
+        }
+        return this._default_init_database;
+    }
+
+    private get _default_add_user(): string{
+        return `
+            INSERT INTO \`users\`
+                (\`username\`,\`password\`) 
+            VALUES
+                (
+                    ?,
+                    PASSWORD(?)
+                );
+            `;
+    }
+
+    private get _default_update_user(): string{
+        return `
+            UPDATE 
+                \`users\` 
+            SET 
+                \`password\`= PASSWORD(?) 
+            WHERE 
+                \`username\`=?;
+        `;
+    }
+
+    private get _default_auth_user(): string{
+        return `
+            SELECT
+                u.\`username\`,
+                GROUP_CONCAT(g.\`name\`) AS usergroups
+            FROM
+                \`users\` u
+            LEFT JOIN \`user_group\` ug
+                ON ug.\`username\` = u.\`username\`
+            LEFT JOIN \`groups\` g
+                ON ug.\`group\` = g.\`name\`
+            WHERE
+                u.\`username\` = ?
+                AND u.\`password\` = PASSWORD(?)
+        `;
+    }
+
+    private get _default_init_database(): string[]{
+        return [
+            `CREATE TABLE IF NOT EXISTS \`users\` (
+                \`username\` VARCHAR(255) NOT NULL,
+                \`password\` TEXT NOT NULL,
+                PRIMARY KEY (\`username\`)
+            )
+            COLLATE='utf8_general_ci'
+            ENGINE=InnoDB
+            ;`,
+            `CREATE TABLE IF NOT EXISTS \`groups\` (
+                \`name\` VARCHAR(255) NOT NULL,
+                PRIMARY KEY (\`name\`)
+            )
+            COLLATE='utf8_general_ci'
+            ENGINE=InnoDB
+            ;`,
+            `CREATE TABLE IF NOT EXISTS \`user_group\` (
+                \`username\` VARCHAR(255) NOT NULL,
+                \`group\` VARCHAR(255) NOT NULL,
+                UNIQUE INDEX \`guid_user_guid_group\` (\`username\`, \`group\`),
+                INDEX \`fk_group\` (\`group\`),
+                CONSTRAINT \`fk_group\` FOREIGN KEY (\`group\`) REFERENCES \`groups\` (\`name\`) ON UPDATE CASCADE ON DELETE CASCADE,
+                CONSTRAINT \`fk_user\` FOREIGN KEY (\`username\`) REFERENCES \`users\` (\`username\`) ON UPDATE CASCADE ON DELETE CASCADE
+            )
+            COLLATE='utf8_general_ci'
+            ENGINE=InnoDB
+            ;`
+        ];
+    }
+}
